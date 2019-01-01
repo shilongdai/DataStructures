5337e5a6a987ad965e2d204750956e5a5188df03
Shilong Dai <sdai@viperfish.net>
2018-12-31 20:12:54 -0500

    implemented merge sort with optimizations

--- sequence/sort.py
+++ sequence/sort.py
@@ -19,26 +19,104 @@ def selection_sort(arr):
 
 
 def insertion_sort(arr):
-	length = len(arr)
-	for i in range(1, length):
+	_range_insertion_sort(arr, 0, len(arr))
+
+
+def _range_insertion_sort(array, low, high):
+	length = high - low
+	for i in range(low + 1, high):
 		insertion_point = i
-		insertion_val = arr[i]
-		for j in range(1, i + 1):
-			if insertion_val < arr[i - j]:
-				arr[i - j + 1] = arr[i - j]
-				insertion_point = i - j
-		arr[insertion_point] = insertion_val
+		insertion_val = array[i]
+		while insertion_point > low and array[insertion_point - 1] > insertion_val:
+			array[insertion_point] = array[insertion_point - 1]
+			insertion_point -= 1
+		array[insertion_point] = insertion_val
+
+
+def shell_sort(arr):
+	length = len(arr)
+	h = 1
+	while h < length // 3:
+		h = 3 * h + 1
+	while h >= 1:
+		for i in range(1, length):
+			insertion_point = i
+			insertion_val = arr[i]
+			while insertion_point > 0 and arr[insertion_point - h] > insertion_val:
+				arr[insertion_point] = arr[insertion_point - h]
+				insertion_point -= h
+			arr[insertion_point] = insertion_val
+		h = h // 3
+
+
+def _merge(input_array, aux, low, mid, high):
+	a_pos = low
+	b_pos = mid
+	if mid < high:
+		if aux[mid] >= aux[mid - 1]:
+			for j in range(high - low):
+				input_array[low + j] = aux[low + j]
+			return
+	for i in range(high - low):
+		k = i + low
+		if a_pos == mid:
+			input_array[k] = aux[b_pos]
+			b_pos += 1
+			continue
+		if b_pos >= high:
+			input_array[k] = aux[a_pos]
+			a_pos += 1
+			continue
+		if aux[a_pos] > aux[b_pos]:
+			input_array[k] = aux[b_pos]
+			b_pos += 1
+		else:
+			input_array[k] = aux[a_pos]
+			a_pos += 1
+
+
+def merge_sort(input_array):
+	original = input_array
+	segment_size = 16
+	length = len(input_array)
+
+	# use insertion sort for small sub-array
+	start = 0
+	end = segment_size
+	while end < length:
+		_range_insertion_sort(input_array, start, end)
+		start = end
+		end += segment_size
+	_range_insertion_sort(input_array, start, length)
+
+	aux = [None for i in range(length)]
+	while segment_size < len(input_array):
+		start = 0
+		end = segment_size * 2
+		aux, input_array = input_array, aux
+		while end < length:
+			mid = start + segment_size
+			_merge(input_array, aux, start, mid, end)
+			start = end
+			end += (segment_size * 2)
+		final_mid = start + segment_size
+		_merge(input_array, aux, start, final_mid, length)
+		segment_size = segment_size * 2
+	for i in range(length):
+		original[i] = input_array[i]
 
 
 class SortingTest:
 
 	def test_sort(self):
+		n = 10000
+		random.seed(n)
 		to_sort = []
-		for i in range(100):
-			to_sort.append(random.randint(0, 100))
+		for i in range(n):
+			to_sort.append(random.randint(0, 1000))
 		algorithm = self._get_sorting_algorithm()
 		algorithm(to_sort)
-		for i in range(1, 100):
+		for i in range(1, n):
 			self.assertTrue(to_sort[i] >= to_sort[i - 1], "Sorting algorithm failed to put everything in order")
 
 
@@ -48,7 +126,19 @@ class SelectionSortTest(unittest.TestCase, SortingTest):
 		return selection_sort
 
 
-class InsetionSortTest(unittest.TestCase, SortingTest):
+class InsertionSortTest(unittest.TestCase, SortingTest):
 
 	def _get_sorting_algorithm(self):
 		return insertion_sort
+
+
+class ShellSortTest(unittest.TestCase, SortingTest):
+
+	def _get_sorting_algorithm(self):
+		return shell_sort
+
+
+class MergeSortTest(unittest.TestCase, SortingTest):
+
+	def _get_sorting_algorithm(self):
+		return merge_sort
